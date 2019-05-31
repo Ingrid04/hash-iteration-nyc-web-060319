e44123fe61a8d6ea3cb35730637bbe4c1cb65db6
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 00:58:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,10 @@ def happy_birthday(birthday_kids)
     "Sarah" => 6, 
     "Amanda" => 27    # add your code snippet here!
      }
-     
+     happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
      
 end
 

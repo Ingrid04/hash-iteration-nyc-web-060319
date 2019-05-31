6782a760139d797b8bfed794658e3edf05ea7536
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 00:57:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,13 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+     birthday_kids = {
+    "Timmy" => 9, 
+    "Sarah" => 6, 
+    "Amanda" => 27    # add your code snippet here!
+     }
+     
+     
 end
 
 

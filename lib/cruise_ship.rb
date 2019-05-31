f7a9c735f712d64e5f1db219ac6df2aac58299d1
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 01:00:16 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,10 +18,9 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-    winner = name
+       winner = name
     end
  
-  puts winner
+ winner
  
  end
- 

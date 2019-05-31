5f0b6abdf275c98a6fcba9174aa72d2b339dca60
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 00:56:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,5 +14,14 @@ def select_winner(passengers)
   suite_d: "Charlie Chaplin", 
   suite_e: "Crumpet the Elf"        # add the code snippet here!
   }
+  
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
+winner
+
 
